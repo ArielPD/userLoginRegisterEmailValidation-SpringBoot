82df8ceb0d885fc242f00ddc8b7d1a78ba347302
Ariel Pinto D <ariel@Ariels-MacBook-Pro.local>
2023-02-23 17:58:14 -0400

    inicial project in spring boot

---
+++ src/main/java/com/apd/userLoginRegisterEmailValidationSpringBoot/UserLoginRegisterEmailValidationSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.apd.userLoginRegisterEmailValidationSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginRegisterEmailValidationSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLoginRegisterEmailValidationSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apd/userLoginRegisterEmailValidationSpringBoot/UserLoginRegisterEmailValidationSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apd.userLoginRegisterEmailValidationSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLoginRegisterEmailValidationSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
